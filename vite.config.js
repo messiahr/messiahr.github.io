e3991ca04eb6e185a01ea32728fbf8bf7164919b
Messiah Ridgley II <messiahridgley@brandeis.edu>
2026-02-27 19:46:21 -0500

    Set up GitHub Pages deployment

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+// https://vite.dev/guide/build#multi-page-app
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { defineConfig } from 'vite';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        nested: resolve(__dirname, 'nested/index.html'),
+      },
+    },
+  },
+});
